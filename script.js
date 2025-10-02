11d54aa63811038594c5f123d4af23a2a6b9ecc6
nlmagalhaes <165035204+nlmagalhaes@users.noreply.github.com>
2025-10-01 23:58:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const track = document.querySelector('.carousel-track');
+const btnLeft = document.querySelector('.carousel-btn.left');
+const btnRight = document.querySelector('.carousel-btn.right');
+
+let index = 0;
+const totalCards = document.querySelectorAll('.card').length;
+const cardsPerView = 3;
+
+btnRight.addEventListener('click', () => {
+  if (index < totalCards - cardsPerView) {
+    index++;
+    updateCarousel();
+  }
+});
+
+btnLeft.addEventListener('click', () => {
+  if (index > 0) {
+    index--;
+    updateCarousel();
+  }
+});
+
+function updateCarousel() {
+  const cardWidth = document.querySelector('.card').offsetWidth + 30;
+  track.style.transform = `translateX(-${index * cardWidth}px)`;
+}
